0c12987954a34a939b1f7b7e54e30daf78eb0b35
Vendico07 <106927930+Vendico07@users.noreply.github.com>
2023-12-08 00:48:37 +0700

    Bikin Booking system
    tinggal ubah delet btn dan bikin admin

---
+++ doctor-appointment-booking-system-php/top_menusUser.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-fixed-top navbar-toggleable-sm navbar-inverse bg-success top-nav-bar">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+     <span class="navbar-toggler-icon"></span>
+    </button>    
+    <div class="navbar-collapse collapse" id="collapsingNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+           <h2><a class="nav-link" href="dashboard.php">Book Your Vaccine <span class="sr-only"></span></a></h2>
+        </li>       
+      </ul> 	  
+	  <ul class="navbar-nav ml-auto">
+	  <li class="nav-item">
+			  <a class="nav-link" href="http://127.0.0.1:5500/Vaksin-main/Vaksin/Tes.html">Back to Home</a> 
+		<?php if(!empty($_SESSION) && $_SESSION["userid"]) { ?>
+			<li class="nav-item">
+				
+			</li>		
+			<li class="nav-item">
+			  <a class="nav-link" href="http://127.0.0.1:5500/Vaksin-main/Vaksin/Tes.html">Back to Home</a>         
+			</li>        
+			</li>
+		<?php } ?>
+      </ul>	  
+    </div>
+  </nav>
